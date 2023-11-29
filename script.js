0df4a3efb79e46d14e7242f7d34a9012e71fb864
Jennifer Arianto <Jenniferarianto@gmail.com>
2023-11-30 01:20:50 +0700

    update about me section

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// JavaScript
-
-document.addEventListener("DOMContentLoaded", function () {
-  const projects = document.querySelectorAll(".project");
-  const modal = document.querySelector(".project-modal");
-
-  projects.forEach((project) => {
-    project.addEventListener("click", () => {
-      const projectName = project.querySelector(".project-info h3").textContent;
-      const projectDetails = project.querySelector(".project-info p").textContent;
-      const githubLink = project.querySelector("button").getAttribute("data-github");
-
-      modal.innerHTML = `
-        <div class="project-info">
-          <h3>${projectName}</h3>
-          <p>${projectDetails}</p>
-          ${githubLink ? `<a href="${githubLink}" target="_blank">GitHub Repository</a>` : ''}
-        </div>`;
-      modal.style.display = "flex";
-    });
-  });
-
-  modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll("nav a");
 
